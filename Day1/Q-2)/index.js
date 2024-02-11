354bfb9492d3dcb83c2269c38631db379bf4ec17
riya kumari <139640191+Riya79hp@users.noreply.github.com>
2024-02-11 21:06:16 +0530

    Add files via upload

---
+++ Day1/Q-2)/index.js
@@ -0,0 +1,25 @@
+
+
+function handle(){
+word=document.getElementById('word').value;
+fetch('https://dummyjson.com/posts/search?'+'q='+word)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+       let y=document.getElementById('fetch');
+       y.innerHTML='';
+       let u=json.limit;
+       for(i=0;i<u;i++){
+        let titlei=document.createElement('h');
+        let titler=json.posts[i].title;
+        let r=json.posts[i].body;
+        let c=document.createElement('div');
+        titlei.innerHTML='<b>'+titler+'<b>';
+        c.innerHTML=r;
+        y.appendChild(titlei);
+        y.appendChild(c);
+      }
+       
+       
+      })
+}
